4571eedb9a6cb51e9042833f7900450aa6cfed20
beary <36056927+itsbeary@users.noreply.github.com>
2022-11-26 11:24:32 +1300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BearyValheim
+{
+    [BepInPlugin("beary.BearyValheim", "Beary Valheim", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("beary.BearyValheim");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+        class Jump_Patch
+        {
+            static void Prefix(ref float ___m_jumpForce, ref float ___m_jumpStaminaUsage)
+            {
+                Debug.Log($"Jump force: {___m_jumpForce}");
+                ___m_jumpStaminaUsage = 0;
+                ___m_jumpForce = 50;
+                Debug.Log($"Modified jump force: {___m_jumpForce}");
+            }
+        }
+        [HarmonyPatch(typeof(Character), nameof(Character.SetHealth))]
+        class Health_Patch
+        {
+            static void Prefix(ref float health)
+            {
+                health = 2;
+            }
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.HaveStamina))]
+        public static void HaveStamina(ref bool amount)
+        {
+            amount = true;
+        }
+    }
+}
